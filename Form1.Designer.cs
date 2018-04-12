39b6f59f1494c3ef268dca704618fa00da38a85b
Morozov Damian <web@morozov33.ru>
2018-04-12 16:44:33 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace GridViewFewLines
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.buttonAddRows = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(0, 0);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(624, 442);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // buttonAddRows
+            // 
+            this.buttonAddRows.Location = new System.Drawing.Point(12, 407);
+            this.buttonAddRows.Name = "buttonAddRows";
+            this.buttonAddRows.Size = new System.Drawing.Size(150, 23);
+            this.buttonAddRows.TabIndex = 1;
+            this.buttonAddRows.Text = "Добавить строки";
+            this.buttonAddRows.UseVisualStyleBackColor = true;
+            this.buttonAddRows.Click += new System.EventHandler(this.buttonAddRows_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 442);
+            this.Controls.Add(this.buttonAddRows);
+            this.Controls.Add(this.gridControl1);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private System.Windows.Forms.Button buttonAddRows;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using DevExpress.Utils;
+using DevExpress.XtraEditors.Repository;
+
+namespace GridViewFewLines
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Dictionary<int, string> GetDicSource()
+        {
+            return new Dictionary<int, string>
+            {
+                { 1, @"Лежа на гумне в омете, долго читал — и вдруг возмутило. Опять с раннего утра читаю, опять с книгой в руках! И так изо дня в день, с самого детства! Полжизни прожил в каком-то несуществующем мире, среди людей, никогда не бывших, выдуманных, волнуясь их судьбами, их радостями и печалями, как своими собственными, до могилы связав себя с Авраамом и Исааком, с пелазгами и этрусками, с Сократом и Юлием Цезарем, Гамлетом и Данте, Гретхен и Чацким, Собакевичем и Офелией, Печориным и Наташей Ростовой! И как теперь разобраться среди действительных и вымышленных спутников моего земного существования? Как разделить их, как определить степени их влияния на меня?"},
+                { 2, @"Я читал, жил чужими выдумками, а поле, усадьба, деревня, мужики, лошади, мухи, шмели, птицы, облака — все жило своей собственной, настоящей жизнью. И вот я внезапно почувствовал это и очнулся от книжного наваждения, отбросил книгу в солому и с удивлением и с радостью, какими-то новыми глазами смотрю кругом, остро вижу, слышу, обоняю, — главное, чувствую что-то необыкновенно простое и в то же время необыкновенно сложное, то глубокое, чудесное, невыразимое, что есть в жизни и во мне самом и о чем никогда не пишут как следует в книгах."},
+                { 3, @"Пока я читал, в природе сокровенно шли изменения. Было солнечно, празднично; теперь все померкло, стихло. В небе мало-помалу собрались облака и тучки, кое-где, — особенно к югу, — еще светлые, красивые, а к западу, за деревней, за ее лозинами, дождевые, синеватые, скучные. Тепло, мягко пахнет далеким полевым дождем. В саду поет одна иволга."}
+            };
+        }
+
+        // Настроить колонки
+        private void SetupColumns()
+        {
+            gridView1.PopulateColumns();
+
+            if (gridView1.Columns.Count > 0)
+            {
+                gridView1.Columns[0].Caption = @"ID";
+                gridView1.Columns[0].OptionsColumn.FixedWidth = true;
+                gridView1.Columns[0].Width = 75;
+                gridView1.Columns[0].AppearanceHeader.TextOptions.HAlignment = HorzAlignment.Center;
+                gridView1.Columns[0].AppearanceCell.TextOptions.HAlignment = HorzAlignment.Center;
+            }
+
+            if (gridView1.Columns.Count > 1)
+            {
+                gridView1.Columns[1].Caption = @"TEXT";
+                gridView1.Columns[1].AppearanceHeader.TextOptions.HAlignment = HorzAlignment.Center;
+                gridView1.Columns[1].AppearanceCell.TextOptions.HAlignment = HorzAlignment.Near;
+            }
+
+            // Автовысота строк
+            gridView1.OptionsView.RowAutoHeight = true;
+        }
+
+        // Настроить многострочный режим для колонки текста
+        private void SetupWrapColumns(int col)
+        {
+            if (gridView1.Columns.Count > col)
+            {
+                var repositoryItemMemoEdit = new RepositoryItemMemoEdit
+                {
+                    // Перенос строк
+                    WordWrap = true
+                };
+
+                gridView1.Columns[col].ColumnEdit = repositoryItemMemoEdit;
+            }
+        }
+
+        // Добавить строки
+        private void buttonAddRows_Click(object sender, EventArgs e)
+        {
+            // Источник
+            gridControl1.DataSource = GetDicSource();
+
+            // Настроить колонки
+            SetupColumns();
+
+            // Настроить многострочный режим для колонки текста
+            SetupWrapColumns(1);
+        }
+    }
+}
